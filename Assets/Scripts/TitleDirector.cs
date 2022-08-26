df9bb18323763d188bf97e5d1103b0182f6b0b0f
snowman20TI048 <snowman2000y@gmail.com>
2022-08-26 20:09:01 +0900

    タイトルBGMの追加

--- Assets/Scripts/TitleDirector.cs
+++ Assets/Scripts/TitleDirector.cs
@@ -5,7 +5,17 @@ using UnityEngine.SceneManagement; //LoadSceneを使うために必要！！
 
 public class TitleDirector : MonoBehaviour
 {
-   
+    [Header("メインBGM")]
+    [SerializeField]
+    private AudioManager audioManager;              // ヒエラルキーにある AudioManager スクリプトのアタッチされているゲームオブジェクトをアサイン
+
+    void Start()
+    {
+        // タイトル曲再生
+        StartCoroutine(audioManager.PlayBGM(0));
+    }
+
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Z))
